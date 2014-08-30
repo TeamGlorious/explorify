173b3f19b489c331bae02643eca662ddbf4cdae9
JesseMoore <vergobret@gmail.com>
2014-08-30 16:01:57 -0700

    Drop password column from users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140830225609) do
+ActiveRecord::Schema.define(version: 20140830230001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,11 +19,12 @@ ActiveRecord::Schema.define(version: 20140830225609) do
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "img"
+    t.string   "code"
+    t.datetime "expires_at"
   end
 
 end
