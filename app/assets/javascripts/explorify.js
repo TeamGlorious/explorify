5e717841380b3cabaa0b181bcec0a824112a3d48
salamandermike <salamandermike@gmail.com>
2014-08-31 19:21:15 -0700

    Map Marker Animation

--- app/assets/javascripts/explorify.js
+++ app/assets/javascripts/explorify.js
@@ -1,22 +1,35 @@
 var coordinates = [[37.2,-122.2],[37.3,-121.8],[37.5,-122.1]];
 
+
 function initialize(){
   // var to define boundary of all coordinates
-  var bounds = new google.maps.LatLngBounds();
   var mapOptions = { mapTypeId: google.maps.MapTypeId.TERRAIN };
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-  // place markers on map & extend zoom boundary
-  for (var i = 0; i < coordinates.length; i++){
-    var travelPoint = new google.maps.LatLng(coordinates[i][0], coordinates[i][1]);
-    var marker = new google.maps.Marker({
-      position: travelPoint,
-      map: map,
-      title:"Travel Point Test"
-    });
-    bounds.extend(travelPoint); // zoom boundary
-  }
+  var bounds = new google.maps.LatLngBounds();
+  addMarkers(bounds, map);
   map.fitBounds(bounds); // set center and zoom level
 }
+
+function addMarkers(bounds, map){
+  for (var i = 0, travelPoints = []; i < coordinates.length; i++){
+    var mapBoundary = new google.maps.LatLng(coordinates[i][0], coordinates[i][1]);
+    travelPoints.push(new google.maps.LatLng(coordinates[i][0], coordinates[i][1]));
+
+    var iterate = 0;
+    setTimeout(function(){
+      new google.maps.Marker({
+        position: travelPoints[iterate],
+        map: map,
+        draggable: false,
+        animation: google.maps.Animation.DROP
+      });
+      iterate++;
+    }, i * 800);
+
+    bounds.extend(mapBoundary); // zoom boundary
+  }
+}
+
 
 // Creates javascript tags and calls initialize function
 function loadScript() {
@@ -29,3 +42,9 @@ function loadScript() {
 window.onload = loadScript;
 
 
+    // var marker = new google.maps.Marker({
+    //   position: travelPoint,
+    //   map: map,
+    //   animation: google.maps.Animation.DROP,
+    //   title:"Travel Point Test"
+    // });
