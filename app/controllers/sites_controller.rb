df25fa5c2a491d228e7546333cf641ff1c948f50
JesseMoore <vergobret@gmail.com>
2014-09-04 17:51:18 -0700

    Navbar finalized.  About and Contact pages added

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,2 +1,13 @@
 class SitesController < ApplicationController
+  def index
+    @current_user = current_user
+  end
+
+  def about
+    render "/sites/about"
+  end
+
+  def contact
+    render "/sites/contact"
+  end
 end
