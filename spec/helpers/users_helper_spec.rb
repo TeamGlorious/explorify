c1706a9f2e8c4a33ee7fddcdf5041b4b07040c80
salamandermike <salamandermike@gmail.com>
2014-09-08 08:00:37 -0700

    rspec

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -11,11 +11,9 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe UsersHelper, :type => :helper do
-
   it "Should create a new user without saving to database" do
     mike = User.new(name: "Mike", email: "me@me.com")
     expect(mike.name).to eq("Mike")
     expect(mike.new_record?).to eq(true)
   end
-
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Trip, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "Should create a new trip without saving to database" do
+    trip = Trip.new(title: "My Trip", date_start: "2013-01-01", date_end: "2013-01-14")
+    expect(trip.title).to eq("My Trip")
+    expect(trip.date_start).to eq("2013-01-01")
+    expect(trip.date_end).to eq("2013-01-14")
+    expect(trip.new_record?).to eq(true)
+  end
 end
+
